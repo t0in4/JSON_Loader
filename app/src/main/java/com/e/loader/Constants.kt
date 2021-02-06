15f706f98586fd89d635f409b8734015aa8c1d11
t0in4 <t0in4@yahoo.com>
2021-02-06 17:44:26 +0300

    done

---
+++ app/src/main/java/com/e/loader/Constants.kt
@@ -0,0 +1,12 @@
+package com.e.loader
+
+object Constants {
+    const val MARKETING: String = "markenting"
+    const val BUSINESS: String = "business"
+    const val CODING: String = "coding"
+    const val DESIGN: String = "design"
+    const val ANALYTIC: String = "analytic"
+    const val MBA: String = "mba"
+
+
+}
--- app/src/main/java/com/e/loader/MainActivity.kt
+++ app/src/main/java/com/e/loader/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.e.loader
 
 import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
@@ -13,16 +22,28 @@ import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
 import java.net.URL
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        CallAPILoginAsyncTask().execute()
+
+        val spannable =  SpannableString("Изучайте\nактуальные темы")
+        spannable.setSpan(
+                ForegroundColorSpan(Color.RED),
+                9, // start
+                24, // end
+                Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+        )
+        text_view.text = spannable
+        CallTask().execute()
+
 
 
 
     }
-    private inner class CallAPILoginAsyncTask(): AsyncTask<Any, Void, String>(){
+    private inner class CallTask(): AsyncTask<Any, Void, String>(){
 
         private lateinit var customProgressDialog: Dialog
         override fun onPreExecute() {
@@ -86,11 +107,96 @@ class MainActivity : AppCompatActivity() {
             super.onPostExecute(result)
             cancelProgressDialog()
             Log.i("JSON RESPONSE RESULT", result.toString())
-
             val jsonObject = JSONObject(result)
-            val message = jsonObject.optString("title")
-            Log.i("Title", message)
+            val data = jsonObject.optJSONArray("data")
+            val need = mutableListOf<String>()
+            for (i in 0 until data.length()) {
+                var counter = 0
+                need.clear()
+                var ourObject: JSONObject = data[i] as JSONObject
+                val groups = ourObject.getJSONArray("groups")
+                val direction = ourObject.getJSONObject("direction")
+                val directionTitle = direction.optString("title")
+                println(directionTitle)
+                for (z in 0 until groups.length()) {
+                    //println("working")
+                    val itemsObject: JSONObject = groups[z] as JSONObject
+                    //println("working")
+                    val items = itemsObject.getJSONArray("items")
+                    //println("working")
+                    for (x in 0 until items.length()) {
+                        //println("working")
+                        //println(items[x])
+                        counter++
+                        val titleObject: JSONObject = items[x] as JSONObject
+
+                        val titleString = titleObject.optString("title")
+                        //println(titleString)
+                        need.add(titleString)
+
+                    }
+
+                }
+
+                /*println(need.size)
+                println(need)
+                println(counter)*/
+                when(directionTitle) {
+                    "Маркетинг" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.MARKETING, need.size.toString())
+                        val username = intent.getStringExtra(Constants.MARKETING)
+                        marketing.text = username
+                        startActivity(intent)
+                        finish()
+
+                    }
+                    "Бизнес и управление" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.BUSINESS, need.size.toString())
+                        val username = intent.getStringExtra(Constants.BUSINESS)
+                        business.text = username
+                        startActivity(intent)
+                        finish()
+                    }
+                    "Программирование" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.CODING, need.size.toString())
+                        val username = intent.getStringExtra(Constants.CODING)
+                        coding.text = username
+                        startActivity(intent)
+                        finish()
+                    }
+                    "Дизайн и UX" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.DESIGN, need.size.toString())
+                        val username = intent.getStringExtra(Constants.DESIGN)
+                        design.text = username
+                        startActivity(intent)
+                        finish()
+                    }
+                    "Аналитика" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.ANALYTIC, need.size.toString())
+                        val username = intent.getStringExtra(Constants.ANALYTIC)
+                        analytic.text = username
+                        startActivity(intent)
+                        finish()
+                    }
+                    "MBA" -> {
+                        val intent = Intent(this@MainActivity, MainActivity::class.java)
+                        intent.putExtra(Constants.MBA, need.size.toString())
+                        val username = intent.getStringExtra(Constants.MBA)
+                        mba.text = username
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+                }
         }
+
+
+
 
         private fun showProgressDialog() {
             customProgressDialog = Dialog(this@MainActivity)
@@ -100,6 +206,7 @@ class MainActivity : AppCompatActivity() {
         private fun cancelProgressDialog() {
             customProgressDialog.dismiss()
         }
+
 
     }
 
